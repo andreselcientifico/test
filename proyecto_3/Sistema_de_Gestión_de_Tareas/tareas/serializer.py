b1ed0ebe529b7ad2043a12f1d91af1a9734ae0ba
andreselcientifico <andresperezrodriguez28@gmail.com>
2024-01-06 20:31:57 -0500

    validations were added to the api

--- proyecto_3/Sistema_de_Gestión_de_Tareas/tareas/serializer.py
+++ proyecto_3/Sistema_de_Gestión_de_Tareas/tareas/serializer.py
@@ -6,3 +6,22 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tarea
         fields = '__all__'
+
+    def validate_titulo(self, value):
+        # Validación personalizada para el campo 'titulo'
+        if not value:
+            raise serializers.ValidationError("El título no puede estar vacío.")
+        return value
+
+    def validate_descripcion(self, value):
+        # Validación personalizada para el campo 'descripcion'
+        if not value:
+            raise serializers.ValidationError("La descripción no puede estar vacía.")
+        return value
+
+    def validate_estado(self, value):
+        # Validación personalizada para el campo 'estado'
+        choices = dict(Tarea.ESTADO_CHOICES)
+        if value not in choices:
+            raise serializers.ValidationError(f"El estado debe ser uno de los siguientes valores: {', '.join(choices.keys())}.")
+        return value
